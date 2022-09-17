47c753dbd916293ec927aa850840be43409e8cdc
ekubay <axuTec14@gmail.com>
2022-09-17 22:28:22 +0300

    script file

--- scripts/Preprocessing.py
+++ scripts/Preprocessing.py
@@ -1,153 +1,56 @@
-import numpy as np
-import pandas as pd
-from data_info import DataInfo
+import json
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join("./Scripts/")))
 
 
-class dataPreprocess:
-    def __init__(self, df: pd.DataFrame):
-        self.df = df
+class DataPreprocessor():
+    def __init__(self) -> None:
+        pass
+
+    def remove_irrelevant_pairs(self, data, key):
+       
+        try:
+            del data[key]
+        except Exception as e:
+            pass
         
-        print('Cleaning ...')
-
-    def drop_duplicate(self, df: pd.DataFrame) -> pd.DataFrame:
-        """
-        drop duplicate rows
-        """
-        df.drop_duplicates(inplace=True)
-
-        return df
-
-    def convert_to_datetime(self, df: pd.DataFrame) -> pd.DataFrame:
-        """
-        convert column to datetime
-        """
-
-        df['start'] = pd.to_datetime(
-            df['start'])
-        df['end'] = pd.to_datetime(
-            df['end'])
-
-        return df
-
-    def drop_columns_with_null_values(self, df: pd.DataFrame, threshold_in_percent=30) -> pd.DataFrame:
-        Helper = DataInfo()
-
-        null_percent_df = pd.DataFrame(columns=['column', 'null_percent'])
-        columns = df.columns.values.tolist()
-
-        null_percent_df['column'] = columns
-        null_percent_df['null_percent'] = null_percent_df['column'].map(
-            lambda x: Helper.percent_missing_for_col(df, x))
-
-        columns_to_be_dropped = null_percent_df[null_percent_df['null_percent']
-                                                > threshold_in_percent]['column'].to_list()
-        df = self.__drop_columns(df, columns_to_be_dropped)
-
-        return df
-
-    def drop_rows_with_null_values(self, df: pd.DataFrame, threshold_in_percent=1) -> pd.DataFrame:
-        Helper = DataInfo()
-
-        null_percent_df = pd.DataFrame(columns=['column', 'null_percent'])
-        columns = df.columns.values.tolist()
-
-        null_percent_df['column'] = columns
-        null_percent_df['null_percent'] = null_percent_df['column'].map(
-            lambda x: Helper.percent_missing_for_col(df, x))
+    def clean_tokens(self, data, irrelevant_keys=['start', 'end', 'token_start', 'token_end']):
         
-        columns_subset = null_percent_df[null_percent_df['null_percent']
-                                         < threshold_in_percent]['column'].to_list()
+        try:
+            final_data = []
+            for d in data:
+                temp_data = d
+                temp_tokens = []
+                for token in d['tokens']:
+                    temp_token = {}
+                    for k in token.keys():
+                        if k not in irrelevant_keys:
+                            temp_token[k] = token[k]
+                    temp_tokens.append(temp_token)
+                temp_data['tokens'] = temp_tokens
+                final_data.append(temp_data)
+            
+        except Exception:
+            print("--> failed...")
 
-        df = df.dropna(subset=columns_subset)
 
-        return df
+    def runpipeline(self, data, irrelevant_data_keys=[], irrelevant_token_keys=[]):
+        for df in data:
+            for key in irrelevant_data_keys:
+                self.remove_irrelevant_pairs(df, key)
 
-    def handle_missing_qantitative_data_with_mean(self, df: pd.DataFrame, method="mean"):
+        self.clean_tokens(data, irrelevant_keys=irrelevant_token_keys)
 
-        numeric_data = ['int16', 'int32', 'int64',
-                        'float16', 'float32', 'float64']
+    def Punc(self, reg, stoken):
+        reg = re.compile('(@[a-z0-9]+)|([^0-9a-z \t])|(\w+:\/\/\S+)')
+        no_punc = []
+        for x in stoken:
+            review = []
+            for token in x:
+                new_token = reg.sub(u'', token)
+                if not new_token == u'':
+                    self.review.append(new_token)
+            self.no_punc.append(review)
 
-        all_cols = df.columns.to_list()
-        num_cols = [c for c in all_cols if df[c].dtypes in numeric_data]
-
-        if (method == "mean"):
-
-            for col in num_cols:
-                df[col] = df[col].fillna(df[col].mean())
-
-            return df
-
-        elif method == "ffill":
-
-            for col in num_cols:
-                df[col] = df[col].fillna(method='ffill')
-
-            return df
-
-        elif method == "bfill":
-
-            for col in num_cols:
-                df[col] = df[col].fillna(method='bfill')
-
-            return df
-        else:
-            print("Method unknown")
-            return df
-    
-    def handle_missing_categorical_data_with(self, df: pd.DataFrame, method="ffill"):
-
-        numeric_data = ['int16', 'int32', 'int64',
-                        'float16', 'float32', 'float64']
-
-        all_cols = df.columns.to_list()
-        num_cols = [c for c in all_cols if not df[c].dtypes in numeric_data]
-        
-        if method == "ffill":
-
-            for col in num_cols:
-                df[col] = df[col].fillna(method='ffill')
-
-            return df
-
-        elif method == "bfill":
-
-            for col in num_cols:
-                df[col] = df[col].fillna(method='bfill')
-
-            return df
-        else:
-            print("Method unknown")
-            return df
-
-    def handle_outliers(self, df, col):
-        df = df.copy()
-        q1 = df[col].quantile(0.25)
-        q3 = df[col].quantile(0.75)
-
-        lower_bound = q1 - ((1.5) * (q3 - q1))
-        upper_bound = q3 + ((1.5) * (q3 - q1))
-
-        df[col] = np.where(df[col] < lower_bound, lower_bound, df[col])
-        df[col] = np.where(df[col] > upper_bound, upper_bound, df[col])
-
-        return df
-
-    #Treating the outliers 
-    # a function for replacing outliers with the median, used when there's too many outliers in a feature
-    def replace_outlier_with_median(dataFrame, feature):
-        Q1 = dataFrame[feature].quantile(0.25)
-        Q3 = dataFrame[feature].quantile(0.75)
-        median = dataFrame[feature].quantile(0.50)
-
-        IQR = Q3 - Q1
-
-        upper_whisker = Q3 + (1.5 * IQR)
-        lower_whisker = Q1 - (1.5 * IQR)
-
-        dataFrame[feature] = np.where(dataFrame[feature] > upper_whisker, median, dataFrame[feature])
-        dataFrame[feature] = np.where(dataFrame[feature] < lower_whisker, median, dataFrame[feature])
-        return 
-        
-    def __drop_columns(self, df, columns=[]):
-
-        return df.drop(columns, axis=1)
